63a05377f57564f9fb48a1fdb90f697899739048
Shi Sha <emailtotheowner@gmail.com>
2020-11-01 16:17:38 -0500

    BookList creating part completed out of CRUD

--- BookListRazor/Model/Book.cs
+++ BookListRazor/Model/Book.cs
@@ -12,6 +12,9 @@ namespace BookListRazor.Model
       public int Id { get; set; }
   [Required]
     public string Name { get; set; }
+
     public string Author { get; set; }
+
+    public string ISBN { get; set; }
   }
 }
