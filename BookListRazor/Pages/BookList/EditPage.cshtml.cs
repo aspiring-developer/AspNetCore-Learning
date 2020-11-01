5123de7523df144996d983f4f7170f216558a1d5
Shi Sha <emailtotheowner@gmail.com>
2020-11-01 18:19:47 -0500

    Post method complete for Edit functionality

--- BookListRazor/Pages/BookList/EditPage.cshtml.cs
+++ BookListRazor/Pages/BookList/EditPage.cshtml.cs
@@ -22,5 +22,24 @@ namespace BookListRazor.Pages.BookList
     {
       Book = await _db.Book.FindAsync(id);
     }
+
+    public async Task<IActionResult> OnPost()
+
+    {
+      if(ModelState.IsValid)
+      {
+        var BookFromDb = await _db.Book.FindAsync(Book.Id);
+        BookFromDb.Name = Book.Name;
+        BookFromDb.Author = Book.Name;
+        BookFromDb.ISBN = Book.Name;
+
+        await _db.SaveChangesAsync();
+
+        return RedirectToPage("Index");
+
+      }
+      return RedirectToPage();
+    }
+
   }
 }
