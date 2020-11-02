a33f07257a6f18d6d16dcfe83c6b555ddc589f44
Shi Sha <emailtotheowner@gmail.com>
2020-11-01 23:02:06 -0500

    CRUD complete

--- BookListRazor/Pages/BookList/Index.cshtml.cs
+++ BookListRazor/Pages/BookList/Index.cshtml.cs
@@ -9,17 +9,30 @@ using Microsoft.EntityFrameworkCore;
 
 namespace BookListRazor.Pages.BookList
 {
-    public class IndexModel : PageModel
-    {
+  public class IndexModel : PageModel
+  {
     private readonly ApplicationDbContext _db;
     public IndexModel(ApplicationDbContext db)
     {
-  _db = db;
+      _db = db;
     }
     public IEnumerable<Book> Books { get; set; }
     public async Task OnGet()
-        {
+    {
       Books = await _db.Book.ToListAsync();
     }
+    public async Task<IActionResult> OnPostDelete(int id)
+    {
+      var book = await _db.Book.FindAsync(id);
+      if (book == null)
+      {
+        return NotFound();
+      }
+      _db.Book.Remove(book);
+      await _db.SaveChangesAsync();
+
+      return RedirectToPage("Index");
     }
+
+  }
 }
